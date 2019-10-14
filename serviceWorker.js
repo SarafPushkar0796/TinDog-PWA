ebb693b0bf91ca9ecfa0e9cfbc93c74a4ed10279
Pushkar Saraf <pushkars919@gmail.com>
2019-10-14 14:56:16 +0530

    Tinder for Dogs static PWA complete.

---
+++ serviceWorker.js
@@ -0,0 +1,40 @@
+const staticCacheName = 'site-static-v1';
+
+const assets = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './css/bootstrap.css',
+  'https://fonts.googleapis.com/css?family=Montserrat:400,900|Ubuntu',
+];
+
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
